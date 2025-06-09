bebefab0315fc2ceaeb5a97adc58ad985424297e
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-06-09 23:17:12 +0300

    Initial Commit

---
+++ PersonalQuiz/Models/Question.swift
@@ -0,0 +1,78 @@
+//
+//  Question.swift
+//  PersonalQuiz
+//
+//  Created by Alexandr Artemov (Mac Mini) on 06.06.2025.
+//
+
+struct Question {
+    let title: String
+    let type: ResponseType
+    let answers: [Answer]
+    
+    static func getQuestions() -> [Question] {
+        [
+            Question(
+                title: "Какую пищу вы предпочитаете?",
+                type: .single,
+                answers: [
+                    Answer(title: "Стейк", animal: .dog),
+                    Answer(title: "Рыба", animal: .cat),
+                    Answer(title: "Морковь", animal: .rabbit),
+                    Answer(title: "Кукуруза", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "Что вам нравиться больше?",
+                type: .multiple,
+                answers: [
+                    Answer(title: "Плавать", animal: .dog),
+                    Answer(title: "Спать", animal: .cat),
+                    Answer(title: "Обниматься", animal: .rabbit),
+                    Answer(title: "Есть", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "Любите ли вы поездки на машине?",
+                type: .ranged,
+                answers: [
+                    Answer(title: "Ненавижу", animal: .cat),
+                    Answer(title: "Нервничаю", animal: .rabbit),
+                    Answer(title: "Не замечаю", animal: .turtle),
+                    Answer(title: "Обожаю", animal: .dog)
+                ]
+            ),
+        ]
+    }
+}
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
+
+struct Answer {
+    let title: String
+    let animal: Animal
+}
+
+enum Animal: Character {
+    case dog = "🐶"
+    case cat = "🐱"
+    case rabbit = "🐰"
+    case turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            "Вам нравится быть с друзьями. Вы окружаете себя людьми, которые вам нравятся и всегда готовы помочь."
+        case .cat:
+            "Вы себе на уме. Любите гулять сами по себе. Вы цените одиночество."
+        case .rabbit:
+            "Вам нравится всё мягкое. Вы здоровы и полны энергии."
+        case .turtle:
+            "Ваша сила - в мудрости. Медленный и вдумчивый выигрывает на больших дистанциях."
+        }
+    }
+}
---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexandr Artemov on 06.06.2025.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func doneButtonAction(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+}
