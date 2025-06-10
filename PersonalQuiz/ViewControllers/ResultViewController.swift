998612f462d34a3e01bce2e18b6ac2346cb1103f
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-06-10 17:23:44 +0300

    Была написана логика для определения часто встречающихся типов животных. Также результаты были связаны с соответствующими ULabel.

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -9,15 +9,33 @@ import UIKit
 
 final class ResultViewController: UIViewController {
 
+    @IBOutlet var animalEmojiLabel: UILabel!
+    @IBOutlet var animalDescriptionLabel: UILabel!
+    
     var answers: [Answer]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.hidesBackButton = true
+        findMostFrequentAnimal()
     }
 
     @IBAction func doneButtonAction(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
+    }
+    
+    private func findMostFrequentAnimal() {
+        let animalCounts = answers.map{ $0.animal }
+            .reduce(into: [:]) { result, animal in
+            result[animal, default: 0] += 1
+        }
+        
+        if let mostFrequent = animalCounts.max(by: { $0.value < $1.value }) {
+            let type = mostFrequent.key
+            
+            animalEmojiLabel.text = "Вы - \(type.rawValue)"
+            animalDescriptionLabel.text = type.definition
+        }
     }
 }
